3bd84df7c95bd2689b44d0d44da67b38e28da2ee
Esmodea Burk <esmodearburk@gmail.com>
2023-09-08 17:55:34 -0500

    updated gitignore and created react app

---
+++ iag-fe/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        This is a blank page
+      </header>
+    </div>
+  );
+}
+
+export default App;
