294729014d2110442a0f90fe633137bea7701093
Esmodea Burk <esmodearburk@gmail.com>
2023-09-08 19:48:51 -0500

    set site to auto nav to web above a certain width

--- iag-fe/src/App.js
+++ iag-fe/src/App.js
@@ -1,16 +1,31 @@
 import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './App.css';
 import BottomBar from './application/mobile/components/common/BottomBarMobile';
 import './application/common/common.css';
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+
+  const isMobile = width <= 768;
+  
   return (
     <Routes>
       <Route exact path='/m' element={<Navigate to='/m/home' />} />
       <Route exact path='/' element={<Navigate to='/home' />} />
-      <Route path='/m/home' element={<BottomBar />}/>
-      <Route path='/m/shop' element={<BottomBar />}/>
-      <Route path='/m/user' element={<BottomBar />}/>
+      <Route path='/m/home' element={isMobile ? <BottomBar /> : <></>}/>
+      <Route path='/m/shop' element={isMobile ? <BottomBar /> : <></>}/>
+      <Route path='/m/user' element={isMobile ? <BottomBar /> : <></>}/>
     </Routes>
   );
 }
