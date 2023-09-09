9fbad8673cccab6847d1761f853a530cfda69d6c
Esmodea Burk <esmodearburk@gmail.com>
2023-09-08 19:42:05 -0500

    Finished creating mobile endpoints

--- iag-fe/src/App.js
+++ iag-fe/src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import './App.css';
 import BottomBar from './application/mobile/components/common/BottomBarMobile';
+import './application/common/common.css';
 
 function App() {
   return (
     <Routes>
-      <Route path='/m/home' element={<BottomBar />}/>
+      <Route exact path='/m' element={<Navigate to='/m/home' />} />
       <Route exact path='/' element={<Navigate to='/home' />} />
+      <Route path='/m/home' element={<BottomBar />}/>
+      <Route path='/m/shop' element={<BottomBar />}/>
+      <Route path='/m/user' element={<BottomBar />}/>
     </Routes>
   );
 }
