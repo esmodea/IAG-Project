f8d5095246d87798c695ef31ebf8ff39478151d4
Esmodea Burk <esmodearburk@gmail.com>
2023-09-11 22:26:37 -0500

    Created InfoRow

--- iag-fe/src/application/mobile/pages/Home/InfoRow.js
+++ iag-fe/src/application/mobile/pages/Home/InfoRow.js
@@ -1,13 +1,17 @@
 import React from "react";
+import LinkContainer from "./LinkContainer";
 
-const ArticleMobile = (props) => {
-    const [article] = props;
+const InfoRow = (props) => {
+    const {icon, author, links} = props;
+    console.log(icon, links, author)
 
     return (
-        <>
-        
-        </>
+        <div className="info-row">
+            {icon ? <image src={icon} /> : <div className="fake-image" />}
+            <h3 className="author-name">{author}</h3>
+            {links ? <LinkContainer links={links} /> : <></>}
+        </div>
     )
 }
 
-export default ArticleMobile
+export default InfoRow
