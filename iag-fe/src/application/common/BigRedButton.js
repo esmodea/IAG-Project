3238d1ef9e368ce2b193f5262ec0355265dbda80
Esmodea Burk <esmodearburk@gmail.com>
2023-09-11 21:01:56 -0500

    Gave button icons a classname and gave buttons animation

--- iag-fe/src/application/common/BigRedButton.js
+++ iag-fe/src/application/common/BigRedButton.js
@@ -4,16 +4,14 @@ import './common.css'
 const BigRedButton = (props) => {
     let icons = props.icons.map((item) => {return(item)});
     let text = props.text.map((item) => {return(item)});
-    useEffect(() => {
-        // if(typeof props.icons == 'array'){ let iconArray = [...icons.icons]};
-    }, [])
+
     return (
         <button className="big-red-button">
             {
                 text[0] ? text.map((item, idx) => {return(<p key={'223' + idx} className="button-text">{`${item}`}</p>)}) : <></>
             }
             {
-                icons[0] ? icons.map((Icon, idx) => {return(<Icon key={'123' + idx} />)}) : <></>
+                icons[0] ? icons.map((Icon, idx) => {return(<Icon key={'123' + idx} className="button-icon" />)}) : <></>
             }
         </button>
     )
