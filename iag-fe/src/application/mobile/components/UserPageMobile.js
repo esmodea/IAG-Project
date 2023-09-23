33d607194d23a7d16afd96446f246c8691124bb0
Esmodea Burk <esmodearburk@gmail.com>
2023-09-22 22:34:25 -0500

    Created empty user page

--- iag-fe/src/application/mobile/components/UserPageMobile.js
+++ iag-fe/src/application/mobile/components/UserPageMobile.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BottomBar from "../components/common/BottomBarMobile";
 import TopBar from "../components/common/TopBarMobile";
+import MUserContent from "../pages/User";
 
 const UserPageMobile = () => {
     return (
         <>
             <TopBar />
+            <MUserContent />
             <BottomBar />
         </>
     )
---
+++ iag-fe/src/application/mobile/pages/User/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './userpagemobile.css'
+
+const MUserContent = (props) => {
+
+    return (
+        <div className="user-mobile">
+        </div>
+    )
+}
+
+export default MUserContent
