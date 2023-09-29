049a86b5e9c47e35e950722a83dbb6c3ee5a5fb8
Esmodea Burk <esmodearburk@gmail.com>
2023-09-28 19:55:53 -0500

    Put together Interaction row and buttons

---
+++ iag-fe/src/application/mobile/pages/Home/InteractionButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './homepagemobile.css';
+
+const InteractionButton = (props) => {
+    const {onClick, className, Icon, amount} = props;
+    console.log(amount)
+
+    return (
+        <div className={`interaction-group ${className ? className : ''}`}>
+            <button onClick={onClick} className="interaction-button">
+                {Icon ? <Icon /> : <></>}
+            </button>
+            {amount ? <p className="interaction-amount">{`${amount}`}</p> : <p style={{display: "none"}}></p>}
+        </div>
+    )
+};
+
+export default InteractionButton;
--- iag-fe/src/application/mobile/pages/Home/InteractionRow.js
+++ iag-fe/src/application/mobile/pages/Home/InteractionRow.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import InteractionButton from "./InteractionButton";
+import { LikeOutlined, LikeFilled, DislikeOutlined, DislikeFilled, MailOutlined, MailFilled, ShareAltOutlined } from "@ant-design/icons";
 
-const ArticleMobile = (props) => {
-    const [article] = props;
+const InteractionRow = (props) => {
+    const {article} = props;
+    const [button, setButton] = useState(
+        {
+            like: false,
+            dislike: false,
+            mail: false
+        }
+        );
+
+    const onChange = (buttonType) => {
+        if(buttonType === 'like'){
+            setButton({like:!button.like, dislike:button.dislike, mail:button.mail})
+        }else if(buttonType === 'dislike'){
+            setButton({like:button.like, dislike:!button.dislike, mail:button.mail})
+        }else if(buttonType === 'mail'){
+            setButton({like:button.like, dislike:button.dislike, mail:!button.mail})
+        }
+    }
 
     return (
-        <>
-        
-        </>
+        <div className="interaction-row">
+        <InteractionButton onClick={() => {onChange('like')}} Icon={button.like ? LikeFilled : LikeOutlined} amount={article.content.interaction.likes} /> <InteractionButton onClick={() => {onChange('dislike')}} Icon={button.dislike ? DislikeFilled : DislikeOutlined} /> <InteractionButton onClick={() => {onChange('mail')}} className={'last-button'} Icon={MailOutlined}/> <InteractionButton Icon={ShareAltOutlined} />
+        </div>
     )
 }
 
-export default ArticleMobile
+export default InteractionRow
