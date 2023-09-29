d541a58a7bc4b64a63fabbbac00eb478e19e1190
Esmodea Burk <esmodearburk@gmail.com>
2023-09-28 19:54:56 -0500

    Changed ArticleMobile 's structure

--- iag-fe/src/application/mobile/pages/Home/ArticleMobile.js
+++ iag-fe/src/application/mobile/pages/Home/ArticleMobile.js
@@ -1,13 +1,17 @@
 import React from "react";
 import InfoRow from "./InfoRow";
+import InteractionRow from "./InteractionRow";
 
 const ArticleMobile = (props) => {
     const {article} = props;
 
     return (
-        <>
+        <div className="article-group">
             <InfoRow icon={article.content.info.icon} links={article.content.info.links} author={article.content.info.author} />
-        </>
+            <h3 className="headline">{article.content.article.headline}</h3>
+            <p className="article">{article.content.article.text}</p>
+            <InteractionRow article={article}/>
+        </div>
     )
 }
 
