072a004e23b894e891993be9754310c5e2bf3f45
Esmodea Burk <esmodearburk@gmail.com>
2023-10-17 22:06:46 -0500

    Created homepage skeleton

---
+++ iag-fe/src/application/web/components/Home/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../common/Header";
+import './homepageweb.css'
+
+const WHomeContent = () => {
+
+    return(
+        <div className="home-web">
+            <Header />
+            <div className="page-layout-div">
+                <div className="page-left">
+
+                </div>
+                <div className="page-center">
+
+                </div>
+                <div className="page-right">
+
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default WHomeContent;
--- iag-fe/src/application/web/pages/HomePageWeb.js
+++ iag-fe/src/application/web/pages/HomePageWeb.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "../components/common/Header";
+import WHomeContent from "../components/Home";
 
 const HomePageWeb = () => {
 
     return(
-        <Header />
+        <WHomeContent />
     )
 }
 
