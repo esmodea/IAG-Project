40cca1dd9903e6135ea930d47eaafb075bcf7a61
Esmodea Burk <esmodearburk@gmail.com>
2023-11-04 20:35:15 -0500

    Created writer page for programmatic article displaying

--- iag-fe/src/application/web/components/WriterPage/index.js
+++ iag-fe/src/application/web/components/WriterPage/index.js
@@ -1,32 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../common/Header";
 import './writerpageweb.css'
 import WritersWidget from "../common/WritersWidget";
 import AboutWidget from "../common/AboutWidget";
 import ArticleWeb from "../common/Article";
+import articles from "../../../mock-data/homeArticles";
 
 const WWriterContent = () => {
-
-    // const state = useSelector((state) => state.currentWriter)
+    const writerState = useSelector((state) => state.currentWriter);
+    const location = useLocation();
 
     return(
-        <div className="home-web">
+        <div className="writer-web">
             <Header />
             <div className="page-layout-div">
                 <div className="page-left">
                     <WritersWidget />
                 </div>
-                <div className="writer-body">
-                    <div className="writer-header">
-                        <div className="icon"></div>
-                        <h1>Author Name</h1>
-                    </div>
-                    <div className="page-center">
-                        <ArticleWeb />
-                    </div>
-                    <div className="page-right">
-                        <AboutWidget text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
-                    </div>
+                <div className="page-center">
+                    {articles.map((post) => {
+                        const {article, info, interaction} = post.content;
+                        const authorName = location.pathname.split('/')[location.pathname.split('/').length - 1];
+                        if(info.author == authorName)return <ArticleWeb headline={article.headline} text={article.text} info={info} interaction={interaction} key={post.key} />;
+                    })}
+                    <div className="layout-div">how did you find this?</div>
+                </div>
+                <div className="page-right">
+                    <AboutWidget text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
                 </div>
             </div>
         </div>
