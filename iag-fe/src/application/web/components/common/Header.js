6909febbc9ee2ce7fc34da9c84748bff8f4bdb18
Esmodea Burk <esmodearburk@gmail.com>
2023-11-05 23:52:35 -0600

    Made the logo the home button

--- iag-fe/src/application/web/components/common/Header.js
+++ iag-fe/src/application/web/components/common/Header.js
@@ -2,12 +2,13 @@ import React from "react";
 import BigRedButton from "../../../common/BigRedButton";
 import { LoginOutlined } from "@ant-design/icons";
 import './commonWeb.css'
+import { Link } from "react-router-dom";
 
 const Header = () => {
 
     return (
         <div className="header">
-            <div className="icon"></div>
+            <Link to={'/home'} className="icon"></Link>
             <BigRedButton icons={[LoginOutlined]} text={['Login']} className="login-button" />
         </div>
     )
