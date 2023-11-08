1b7eed1ba9f37bdd81a962e5b7d3f0d49d393162
Esmodea Burk <esmodearburk@gmail.com>
2023-11-08 15:13:59 -0600

    Updated big red button to take an onClick function

--- iag-fe/src/application/common/BigRedButton.js
+++ iag-fe/src/application/common/BigRedButton.js
@@ -4,10 +4,11 @@ import './common.css'
 const BigRedButton = (props) => {
     let icons = props.icons.map((item) => {return(item)});
     let text = props.text.map((item) => {return(item)});
+    let {onClick} = props;
     let className = typeof props.className === 'string' ? `${props.className} big-red-button` : 'big-red-button';
 
     return (
-        <button className={className}>
+        <button onClick={() => {if(onClick)onClick();}} className={className}>
             {
                 text[0] ? text.map((item, idx) => {return(<p key={'223' + idx} className="button-text">{`${item}`}</p>)}) : <></>
             }
