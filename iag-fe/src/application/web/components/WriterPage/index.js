2301dd852f4171d5d3bc4758deb873af29f0d842
Esmodea Burk <esmodearburk@gmail.com>
2023-11-16 22:24:02 -0600

    Made Finishing touches on Writer Page

--- iag-fe/src/application/web/components/WriterPage/index.js
+++ iag-fe/src/application/web/components/WriterPage/index.js
@@ -8,6 +8,7 @@ import AboutWidget from "../common/AboutWidget";
 import ArticleWeb from "../common/Article";
 import articles from "../../../mock-data/homeArticles";
 import authors from "../../../mock-data/authorNames";
+import WriterBanner from "./WriterBanner";
 
 const WWriterContent = () => {
     const writerState = useSelector((state) => state.currentWriter);
@@ -21,18 +22,27 @@ const WWriterContent = () => {
                 <div className="page-left">
                     <WritersWidget />
                 </div>
-                <div className="page-center">
-                    {articles.map((post) => {
-                        const {article, info, interaction} = post.content;
-                        const authorName = location.pathname.split('/')[location.pathname.split('/').length - 1];
-                        if(info.id == authorName)return <ArticleWeb article={article} info={info} interaction={interaction} key={post.key} />;
-                    })}
-                    <div className="layout-div">how did you find this?</div>
-                </div>
-                <div className="page-right">
-                    {authors.map((author) => {
-                        if(author.id === location.pathname.split('/')[location.pathname.split('/').length - 1]) return <AboutWidget text={author.about} />;
-                    })}
+                <div className="banner-layout-div">
+                    <div className="page-banner">
+                        <WriterBanner authorName={authors.map((author) => {
+                            if(author.id == location.pathname.split('/')[location.pathname.split('/').length - 1]) return author.name;
+                        })} />
+                    </div>
+                    <div className="page-center-right">
+                        <div className={`page-center`}>
+                            {articles.map((post) => {
+                                const {article, info, interaction} = post.content;
+                                const authorName = location.pathname.split('/')[location.pathname.split('/').length - 1];
+                                if(info.id == authorName)return <ArticleWeb article={article} info={info} interaction={interaction} key={post.key} />;
+                            })}
+                            <div className="layout-div">how did you find this?</div>
+                        </div>
+                        <div className="page-right">
+                            {authors.map((author) => {
+                                if(author.id === location.pathname.split('/')[location.pathname.split('/').length - 1]) return <AboutWidget text={author.about} />;
+                            })}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
