05b463f89d1e45c9beece2cb97baaa21a1b35044
Esmodea Burk <esmodearburk@gmail.com>
2024-01-05 13:45:03 -0600

    Created writer banner

--- iag-fe/src/application/mobile/pages/WriterPageMobile.js
+++ iag-fe/src/application/mobile/pages/WriterPageMobile.js
@@ -4,13 +4,18 @@ import TopBar from "../components/common/TopBarMobile";
 import MWriterContent from "../components/Writer";
 import articles from "../../mock-data/homeArticles";
 import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
 
 const WriterPageMobile = () => {
     const writerState = useSelector((state) => state.currentWriter);
+    const location = useLocation();
 
     return (
         <div id="MobileHomePage">
             <TopBar />
+            <div className="writer-banner">
+                <h1 className="writer-name">{location.pathname.split('/')[location.pathname.split('/').length - 1].split('-').join(' ')}</h1>
+            </div>
             <MWriterContent articles={articles} writerName={writerState} />
             <BottomBar />
         </div>
