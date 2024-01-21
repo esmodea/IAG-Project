7497e53b5c63bd58ba9abcaa77de96dd7a041524
Esmodea Burk <esmodearburk@gmail.com>
2024-01-20 21:58:21 -0600

    Changed successful get response.

--- iag-be/server.js
+++ iag-be/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Successful response.');
+    res.status(200).send('No query');
 })
 
 app.listen(3001, () => console.log('BE is listening on port 3001'));
