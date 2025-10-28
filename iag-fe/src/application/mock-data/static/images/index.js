b0aa00c8618ec831e67c2b1abdcf212182eb1a76
Esmodea <esmodearburk@gmail.com>
2025-10-28 18:03:02 -0500

    Updated images index

--- iag-fe/src/application/mock-data/static/images/index.js
+++ iag-fe/src/application/mock-data/static/images/index.js
@@ -5,13 +5,13 @@ import key8Image from "url:./Key8article.jpg";
 import key9Image from "url:./Key9article.jpg";
 import key10Image from "url:./Key10article.jpg";
 import key11Image from "url:./Key11article.jpg";
-
-const printImages = (images = [emptyIcon, key6Image, key7Image, key8Image, key9Image, key10Image, key11Image,]) => {
-    images.map((image) => {
-        console.log(image);
-    });
-};
-
-const Images = {emptyIcon, key6Image, key7Image, key8Image, key9Image, key10Image, key11Image, printImages}
-
+const Images = {
+emptyIcon,
+key6Image,
+key7Image,
+key8Image,
+key9Image,
+key10Image,
+key11Image,
+}
 export default Images;
