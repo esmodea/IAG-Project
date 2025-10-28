eeadbe5d3c27f4c2d508b00021e970defc32e979
Esmodea <esmodearburk@gmail.com>
2025-10-28 17:59:32 -0500

    Major homeArticles update

--- iag-fe/src/application/web/components/common/LinkContainer.js
+++ iag-fe/src/application/web/components/common/LinkContainer.js
@@ -12,9 +12,17 @@ const LinkContainer = (props) => {
 
     return (
         <div className="citation-links">
-            <EllipsisOutlined className="citation-button" onClick={clickLinksMenu} />
+            {
+                links && links.length > 2 ? 
+                <button className="citation-button" onClick={clickLinksMenu}>{'Links'}</button>
+                : ''
+            }
             <div className={`citation-menu ${displayMenu ? '' : 'hide'}`}>
-                {links ? links.map((link, idx) => {return(<div className="citation-link" key={Math.min((keysData.keys.components.link_container.min + idx * 2) - 1, keysData.keys.components.link_container.max)}><a target="_blank" rel="noreferrer" href={`https://${link}`}>{link}</a></div>)}) : ''}
+                {links && links.length > 2 ? links.map((link, idx) => {
+                    if(link != 'link_start_iag_client' && link != 'link_end_iag_client') {
+                        return(<div className="citation-link" key={Math.min((keysData.keys.components.link_container.min + idx * 2) - 1, keysData.keys.components.link_container.max)}><a target="_blank" rel="noreferrer" href={`https://${link}`}>{link}</a></div>);
+                    }
+                }) : ''}
             </div>
         </div>
     )
