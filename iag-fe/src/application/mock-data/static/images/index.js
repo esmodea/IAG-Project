812cbcb1e45c0d9514a24730e799846f3f3b60c0
Esmodea <esmodearburk@gmail.com>
2025-11-17 02:05:16 -0600

    IAG-Client update >> Date: 2025-11-17T02:05:15.949561, ChangedFiles: iag-fe/src/application/mock-data/homeArticles.js, iag-fe/src/application/mock-data/static/images/index.js,

--- iag-fe/src/application/mock-data/static/images/index.js
+++ iag-fe/src/application/mock-data/static/images/index.js
@@ -7,7 +7,6 @@ import key3Image from "url:./key3Image.jpg";
 import key4Image from "url:./key4Image.jpg";
 import key5Image from "url:./key5Image.jpg";
 import key6Image from "url:./key6Image.jpg";
-import key7Image from "url:./key7Image.21";
 const images = {
 emptyIcon,
 key1Image,
@@ -16,6 +15,5 @@ key3Image,
 key4Image,
 key5Image,
 key6Image,
-key7Image,
 }
 export default images;
