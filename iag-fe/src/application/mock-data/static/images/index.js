c893575d690092855ac3c52aeaca368386832cab
JasonSatyr42 <doug@angellbusiness.com>
2026-02-02 13:08:19 -0600

    IAG-Client update >> Date: 2026-02-02T13:08:18.770601, ChangedFiles: iag-fe/src/application//mock-data/static/images/index.js, iag-fe/src/application//mock-data/static/images/key10Image.jpg,

--- iag-fe/src/application/mock-data/static/images/index.js
+++ iag-fe/src/application/mock-data/static/images/index.js
@@ -10,6 +10,7 @@ import key6Image from "url:./key6Image.jpg";
 import key7Image from "url:./key7Image.jpeg";
 import key8Image from "url:./key8Image.jpg";
 import key9Image from "url:./key9Image.jpg";
+import key10Image from "url:./key10Image.jpg";
 const images = {
 emptyIcon,
 key1Image,
@@ -21,5 +22,6 @@ key6Image,
 key7Image,
 key8Image,
 key9Image,
+key10Image,
 }
 export default images;
