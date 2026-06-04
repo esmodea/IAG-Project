1571336072e2beab5611dd167a5ca0b94d651bd5
JasonSatyr42 <doug@angellbusiness.com>
2026-06-04 15:18:04 -0500

    IAG-Client update >> Date: 2026-06-04T15:18:03.879274, ChangedFiles: iag-fe/src/application/mock-data/static/images/index.js, iag-fe/src/application/mock-data/static/images/index.js, 	iag-fe/src/application/mock-data/static/images/key13Image.png, , no changes added to commit (use "git add" and/or "git commit -a"),

--- iag-fe/src/application/mock-data/static/images/index.js
+++ iag-fe/src/application/mock-data/static/images/index.js
@@ -13,6 +13,7 @@ import key9Image from "url:./key9Image.jpg";
 import key10Image from "url:./key10Image.jpg";
 import key11Image from "url:./key11Image.png";
 import key12Image from "url:./key12Image.png";
+import key13Image from "url:./key13Image.png";
 const images = {
 emptyIcon,
 key1Image,
@@ -27,5 +28,6 @@ key9Image,
 key10Image,
 key11Image,
 key12Image,
+key13Image,
 }
 export default images;
